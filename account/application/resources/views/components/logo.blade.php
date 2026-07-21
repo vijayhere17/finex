5dabaded7df4eef85b3724bc5a58f5855e1f578b
Vijay Sharma <jangidvijay1125@gmail.com>
2026-07-21 15:58:15 +0530

    Update logo

--- account/application/resources/views/components/logo.blade.php
+++ account/application/resources/views/components/logo.blade.php
@@ -1,3 +1,6 @@
-<a href="{{ URL::to('/') }}/" class="gt-logo d-inline-flex align-items-center text-decoration-none">
-    <img src="{{ URL::to('/') }}/assets/images/logo-lg.png" alt="Global Trade" class="gt-logo-img" />
+<a href="/ginance-new/" class="gt-logo d-inline-flex align-items-center text-decoration-none">
+    <img src="/ginance-new/assets/images/logo22.png"
+         width="120"
+         height="120"
+         class="img-circle" />
 </a>
